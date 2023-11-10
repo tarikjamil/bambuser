9556ea8bb8004152477f554a1ddc8658976025d6
tarikjamil <tarikjamill@gmail.com>
2023-11-10 10:08:11 +0100

    video player

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function runSplit() {
   text = new SplitType("[animation=loading-split]", {
     types: "lines, words",
     lineClass: "overflow-hidden",
-    wordClass: "loading-animation-split"
+    wordClass: "loading-animation-split",
   });
 }
 
@@ -58,14 +58,100 @@ function pageLoad() {
   tl.to(".main-wrapper", {
     opacity: 1,
     ease: "Quint.easeOut",
-    duration: 0.5
+    duration: 0.5,
   });
   tl.from("[animation=loading]", {
     y: "20rem",
     opacity: "0",
     stagger: { each: 0.1, from: "start" },
     ease: "Quint.easeOut",
-    duration: 1
+    duration: 1,
   });
 }
 pageLoad();
+
+// video player
+document.addEventListener("DOMContentLoaded", function () {
+  const videoContainers = document.querySelectorAll(".player--video-container");
+
+  videoContainers.forEach((container) => {
+    const video = container.querySelector(".player--video");
+    const playPauseBtn = container.querySelector(".player--playpause");
+    const playIcon = container.querySelector(".player--icon-play");
+    const pauseIcon = container.querySelector(".player--icon-pause");
+    const currentTimeElem = container.querySelector(".player--currenttime");
+    const durationElem = container.querySelector(".player--duration");
+    const progressContainer = container.querySelector(
+      ".player--progresscontainer"
+    );
+    const progressFilled = container.querySelector(".player--progressfilled");
+
+    const overlay = container.querySelector(".player--overlay");
+    const playOverlayButton = container.querySelector(".player--overlay-play");
+    const repeatOverlayButton = container.querySelector(
+      ".player--overlay-repeat"
+    );
+    const videoControls = container.querySelector(".player--video-controls");
+
+    // Initially hide the playPause button
+    playPauseBtn.style.display = "none";
+
+    playPauseBtn.addEventListener("click", function () {
+      if (video.paused) {
+        video.play();
+        playIcon.style.display = "none";
+        pauseIcon.style.display = "";
+      } else {
+        video.pause();
+        playIcon.style.display = "";
+        pauseIcon.style.display = "none";
+      }
+    });
+
+    playOverlayButton.addEventListener("click", function () {
+      video.play();
+      overlay.style.display = "none";
+      videoControls.style.display = "grid"; // set the controls to display as grid
+      playPauseBtn.style.display = "block"; // Make the playPause button visible
+    });
+
+    repeatOverlayButton.addEventListener("click", function () {
+      video.currentTime = 0;
+      video.play();
+      repeatOverlayButton.style.display = "none";
+      overlay.style.display = "none";
+      playPauseBtn.style.display = "block"; // Make the playPause button visible
+    });
+
+    video.addEventListener("ended", function () {
+      overlay.style.display = "flex";
+      repeatOverlayButton.style.display = "";
+      playOverlayButton.style.display = "none";
+      playPauseBtn.style.display = "none"; // Hide the playPause button again
+    });
+
+    video.addEventListener("timeupdate", function () {
+      const percentage = (video.currentTime / video.duration) * 100;
+      progressFilled.style.width = percentage + "%";
+      currentTimeElem.textContent = formatTime(video.currentTime);
+    });
+
+    progressContainer.addEventListener("click", function (e) {
+      const clickPositionInPixels =
+        e.pageX - progressContainer.getBoundingClientRect().left;
+      const clickPositionInPercentage =
+        (clickPositionInPixels / progressContainer.offsetWidth) * 100;
+      video.currentTime = (clickPositionInPercentage / 100) * video.duration;
+    });
+
+    video.addEventListener("loadedmetadata", function () {
+      durationElem.textContent = formatTime(video.duration);
+    });
+  });
+
+  function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = Math.floor(seconds % 60);
+    return `${mins}:${secs < 10 ? "0" : ""}${secs}`;
+  }
+});
