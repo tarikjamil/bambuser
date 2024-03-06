46ac17bf5f8d2774909a0b67a625c69ddf86adaf
tarikjamil <tarikjamill@gmail.com>
2024-03-06 10:34:07 +0100

    minify

--- page/pricing.js
+++ page/pricing.js
@@ -1,8 +1,1 @@
-$(".info-icon").on("mouseenter", function () {
-    $(this).siblings(".bubble_pricing").toggleClass("is--active");
-  });
-  
-  $(".bubble_pricing").on("mouseleave", function () {
-    $(this).toggleClass("is--active");
-  });
-  
+$(".info-icon").on("mouseenter",function(){$(this).siblings(".bubble_pricing").toggleClass("is--active")}),$(".bubble_pricing").on("mouseleave",function(){$(this).toggleClass("is--active")});
