aca2103c5c743ed0cced66f7e0ee82714698d9cc
tarikjamil <tarikjamill@gmail.com>
2024-03-18 15:55:28 +0000

    edits

--- forecast.js
+++ forecast.js
@@ -1,55 +1,3 @@
-$(".navbar--dropdown-trigger").on("click", function () {
-  // Check if window's inner width is below 992 pixels
-  if (window.innerWidth < 992) {
-    // Save the sibling of the toggle we clicked on
-    let targetElement = $(this).siblings(".navbar--dropdown-list");
-    let arrowElement = $(this).find(".dropdown--icon-tablet");
-    let animationDuration = 500;
-
-    if ($(this).hasClass("open")) {
-      // Close the content div if already open
-      targetElement.animate({ height: "0px" }, animationDuration, () => {
-        arrowElement.css("transform", "rotate(0deg)");
-      });
-    } else {
-      // Open the content div if already closed
-      targetElement.css("height", "auto");
-      let autoHeight = targetElement.height();
-      targetElement.css("height", "0px");
-      targetElement.animate({ height: autoHeight }, animationDuration, () => {
-        targetElement.css("height", "auto");
-        arrowElement.css("transform", "rotate(-90deg)");
-      });
-    }
-
-    // Toggle the open class
-    $(this).toggleClass("open");
-  }
-});
-
-// split type
-let text;
-// Split the text up
-function runSplit() {
-  text = new SplitType("[animation=loading-split]", {
-    types: "lines, words",
-    lineClass: "overflow-hidden",
-    wordClass: "loading-animation-split",
-  });
-}
-
-runSplit();
-
-// Update on window resize
-let windowWidth = $(window).innerWidth();
-window.addEventListener("resize", function () {
-  if (windowWidth !== $(window).innerWidth()) {
-    windowWidth = $(window).innerWidth();
-    text.revert();
-    runSplit();
-  }
-});
-
 gsap.registerPlugin(ScrollTrigger);
 
 // On Page Load
@@ -78,23 +26,8 @@ function pageLoad() {
 }
 pageLoad();
 
-// ------- marquee scrolling ---------------------------- //
 
-// marquee is--marquee-scrolling
-const scrollSpeed = 50; // pixels per second, adjust as needed
 
-function updateScrollingSpeed() {
-  document.querySelectorAll(".is--marquee-scrolling").forEach((element) => {
-    const scrollWidth = element.offsetWidth;
-    const duration = scrollWidth / scrollSpeed; // seconds
-
-    element.style.setProperty("--scroll-width", `${scrollWidth}px`);
-    element.style.animationDuration = `${duration}s`;
-  });
-}
-
-document.addEventListener("DOMContentLoaded", updateScrollingSpeed);
-window.addEventListener("resize", updateScrollingSpeed);
 //-----------------  video player ----------------------------//
 // video player
 document.addEventListener("DOMContentLoaded", function () {
