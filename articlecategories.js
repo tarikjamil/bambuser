465b488188d9afaa68504c8335951f0c78ec9913
tarikjamil <tarikjamill@gmail.com>
2024-03-25 09:55:39 +0000

    Update articlecategories.js

---
+++ articlecategories.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper(".is--categories-slider", {
+  direction: "horizontal",
+  slidesPerView: 1, // Default to 1 slide per view for mobile and smaller viewports
+  slidesPerGroup: 1,
+  spaceBetween: "24rem",
+  loop: false,
+  centeredSlides: false,
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Define breakpoints
+  breakpoints: {
+    // When window width is >= 992px
+    992: {
+      slidesPerView: 4,
+      spaceBetween: "24rem",
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
