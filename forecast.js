1f8adc34d9c8b260e7e36a2093f34a5f0407badf
tarikjamil <tarikjamill@gmail.com>
2024-03-27 14:18:55 +0000

    forecast edits

--- forecast.js
+++ forecast.js
@@ -25,7 +25,39 @@ function pageLoad() {
 }
 pageLoad();
 
+// ----------------- faq ------------------------ //
+$(".is--forecastshow-more").on("click", function () {
+  // Close other accordions when opening new one
+  if (!$(this).hasClass("open")) {
+    $(".is--forecastshow-more.open").click();
+  }
+  // Save the sibling of the toggle we clicked on
+  let sibling = $(this).siblings(".forecast-episode-learn-hide");
+  let animationDuration = 500;
 
+  if ($(this).hasClass("open")) {
+    // Close the content div if already open
+    sibling.animate({ height: "0px" }, animationDuration);
+  } else {
+    // Open the content div if already closed
+    sibling.css("height", "auto");
+    let autoHeight = sibling.height();
+    sibling.css("height", "0px");
+    sibling.animate({ height: autoHeight }, animationDuration, () => {
+      sibling.css("height", "auto");
+
+      // Scroll the page to the accordion, leaving 200 pixels from the top
+      $("html, body").animate(
+        {
+          scrollTop: $(this).offset().top - 200,
+        },
+        animationDuration
+      );
+    });
+  }
+  // Open and close the toggle div
+  $(this).toggleClass("open");
+});
 
 //-----------------  video player ----------------------------//
 // video player
